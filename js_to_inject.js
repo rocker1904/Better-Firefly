dd672293a0a49131438a86a17d6a4cb090906f17
Sam Ellis <sam@emberspark.xyz>
2017-08-06 16:28:33 +0100

    comments added

--- js_to_inject.js
+++ js_to_inject.js
@@ -1,11 +1,14 @@
+// Defines background_option so that chrome.storage can change it and makes 1 the default
 var background_option = 1
 chrome.storage.sync.get({
-		background: background_option
+		background: background_option  // Takes the value from the options script and stores it in 'background_option'
 	}, function(items) {
+		// Stores the CSS for each background in a variable
 		var background1 = "body.background-cover{background:#62b8ed url('https://emberspark.xyz/background1.jpg') fixed top center !important}";
 		var background2 = "body.background-cover{background:#62b8ed url('https://emberspark.xyz/background2.jpg') fixed top center !important}";
 		var backgroundBroken = "body.background-cover{background:#62b8ed url('https://emberspark.xyz/backgroundBroken.jpg') fixed top center !important}";
 		
+		// Sets 'background' to the CSS for the chosen background
 		if (items.background == 1){
 			var background = background1;
 		}
@@ -16,6 +19,7 @@ chrome.storage.sync.get({
 			var backgroud = backgroundBroken;
 		}
 		
+		// Adds that CSS to the site
 		var css = document.createElement("style");
 		css.type = "text/css";
 		css.innerHTML = background;
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-// Saves options to chrome.storage.sync.
+// Saves options to chrome.storage.sync
 function save_options() {
   var background_option = document.getElementById('background_option').value;
   chrome.storage.sync.set({
     background: background_option
   }, function() {
-    // Update status to let user know options were saved.
+    // Update status to let user know options were saved
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
@@ -13,8 +13,7 @@ function save_options() {
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Restores selected option from chrome.storage
 function restore_options() {
   chrome.storage.sync.get({
     background: background_option
@@ -22,6 +21,8 @@ function restore_options() {
     document.getElementById('background_option').value = items.background;
   });
 }
+// Runs restoration when page loads
 document.addEventListener('DOMContentLoaded', restore_options);
+// Runs save when save button is clicked
 document.getElementById('save').addEventListener('click',
     save_options);
